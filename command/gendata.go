6295f7984e36f63f29d0d680274185c7d4bcc6ee
yoshwata <yoshwata@yahoo-corp.jp>
2018-06-15 13:34:13 +0900

    remove comments

--- command/gendata.go
+++ command/gendata.go
@@ -15,14 +15,6 @@ func check_regexp(reg, str string) bool {
 
 func CmdGenData(c *cli.Context) {
 	// Write your code here
-    print("hgoe\n")
-    fmt.Printf("c.NArg()        : %+v\n", c.NArg())
-    fmt.Printf("c.Args()        : %+v\n", c.Args())
-    fmt.Printf("c.Args().Get(0) : %+v\n", c.Args().Get(0))
-    fmt.Printf("c.Args()[0]     : %+v\n", c.Args()[0])
-    fmt.Printf("c.FlagNames     : %+v\n", c.FlagNames())
-
-
     var fp *os.File
     var err error
 
@@ -46,18 +38,13 @@ func CmdGenData(c *cli.Context) {
         check_regexp(`^func`, scanner.Text())
         // Instance method
         r := regexp.MustCompile(`^func \(.*\) (\w+)\(.*`)
-        // r := regexp.MustCompile(`^func\s[.*\s\(\)]?(\w+)\(.*`)
         funcName := r.FindAllStringSubmatch(scanner.Text(), -1)
         if len(funcName) > 0 {
-            // fmt.Println(funcName[0][1])
-            // fmt.Println(r.FindAllStringSubmatch(scanner.Text(), -1))
             if check_regexp(`^func`, scanner.Text()) == true {
                 funcStr += scanner.Text() + "\n"
                 for scanner.Scan() {
                     funcStr += scanner.Text() + "\n"
                     if check_regexp(`^}$`, scanner.Text()) == true {
-                        // fmt.Println(funcName[0][1])
-                        // pp.Println(m)
                         m[funcName[0][1]] = funcStr
                         break
                     }
@@ -68,15 +55,11 @@ func CmdGenData(c *cli.Context) {
         r = regexp.MustCompile(`^func\s(\w+)\(.*`)
         funcName = r.FindAllStringSubmatch(scanner.Text(), -1)
         if len(funcName) > 0 {
-            // fmt.Println(funcName[0][1])
-            // fmt.Println(r.FindAllStringSubmatch(scanner.Text(), -1))
             if check_regexp(`^func`, scanner.Text()) == true {
                 funcStr += scanner.Text() + "\n"
                 for scanner.Scan() {
                     funcStr += scanner.Text() + "\n"
                     if check_regexp(`^}$`, scanner.Text()) == true {
-                        // fmt.Println(funcName[0][1])
-                        // pp.Println(m)
                         m[funcName[0][1]] = funcStr
                         break
                     }
@@ -85,7 +68,6 @@ func CmdGenData(c *cli.Context) {
         }
     }
     mm[c.Args()[0]] = m
-    // pp.Print(m)
     if err := scanner.Err(); err != nil {
         panic(err)
     }
